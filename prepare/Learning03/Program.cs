deb0b977a5013f75b3db201492e1a4c02cabdb33
Scipi0s <gamerzach9@gmail.com>
2023-02-12 19:20:35 -0700

    W06 Prepare complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
+
+        Console.WriteLine(f1.GetFraction());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFraction());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetFraction());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
