bdcf1d7b2f281cbbd747382168e2645edb6c545e
Scipi0s <gamerzach9@gmail.com>
2023-02-22 23:17:37 -0700

    Learning 04 complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment work = new Assignment("Zach", "Social Studies");
+        Console.WriteLine(work.GetSummary());
+
+        MathAssignment mathWork = new MathAssignment("Jose","Calculus","7.3","8-11");
+        Console.WriteLine(mathWork.GetSummary());
+        Console.WriteLine(mathWork.GetHomeworkList());
+
+        WritingAssignment bookReport = new WritingAssignment("Jessie", "Writing","Review of War & Peace");
+        Console.WriteLine(bookReport.GetSummary());
+        Console.WriteLine(bookReport.GetWritingInfo());
     }
 }
